9bcb574549ff153b1b71a8556efd13dcc8d9a8ef
th3maid <36008397+th3maid@users.noreply.github.com>
2023-11-14 14:48:01 -0300

    initial commit

---
+++ maid_scheduler/core/maid.py
@@ -0,0 +1,9 @@
+class Maid:
+
+    def __init__(self, maid_nick: str, maid_name: str, maid_job: str, maid_schedule: dict):
+        self.maid_nick = maid_nick
+        self.maid_name = maid_name
+        self.maid_job  = maid_job
+        self.maid_schedule = maid_schedule
+
+    
